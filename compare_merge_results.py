d1350566b03f9f0f5da89dce7a84b5aa997ebdfa
Domth13 <domth13@gmail.com>
2022-07-23 13:52:49 +0200

    Added .txt output

--- compare_merge_results.py
+++ compare_merge_results.py
@@ -14,9 +14,12 @@ OUTPUT_PATH = './output/'
     # project and output 
 name_output = 'merged_result'
 project_name = 'ProjectDemo'
+text_name = 'info'
+info_text = []
 
 OUTPUT_STRING_XLSX = OUTPUT_PATH + project_name + '_' + name_output + '_' + DATE_STRING + '.xlsx'
 OUTPUT_STRING_CSV =  OUTPUT_PATH + project_name + '_' + name_output + '_' + DATE_STRING + '.csv'
+OUTPUT_STRING_TXT = OUTPUT_PATH + text_name + '_' + DATE_STRING + '.txt'
 
     # dataframe columns two versions (look columns up in Excel-File) default Version: _one
 columns_main_dataframe_one = ['record_id', 'included', 'asreview_ranking', 'authors', 
@@ -39,8 +42,9 @@ columns_final_dataframe_two = ['record_id', 'first_authors', 'primary_title', 'p
 merge_on = 'record_id'
 sort_by = 'asreview_ranking_x'
 
-    # mapper
-mapper = {
+
+    # mapper and meanings
+MAPPER = {
     0: 'TWO_REV_MATCH_excluded',
     2: 'TWO_REV_MATCH_included',
     1: 'TWO_REV_DIFF',
@@ -48,11 +52,18 @@ mapper = {
     -99: 'ONE_REV',
     -198: 'NO_REV',
 }
+
+MEANINGS = ['Meanings', 'NO_REV: not reviewed', 'ONE_REV: reviewed by one reviewer', 
+            'TWO_REV_MATCH_included: included Reviewers match','TWO_REV_MATCH_excluded: excluded Reviewers match',
+             'TWO_REV_DIFF: Rating differs']
+
+SEPERATE = '_________________________'
+
 ######################################################################################
 # functions and main 
 
 def load_dataframes():
-    print('_________________________')
+    print(SEPERATE)
     print('...loading dataframes...' )
     print('')
     input_files = os.listdir(INPUT_PATH)
@@ -65,10 +76,16 @@ def load_dataframes():
     read_merge = pd.read_excel(input_file[1])
     
     df_main = pd.DataFrame(read_main, columns=columns_main_dataframe_one)
-    print('loaded main dataframe. Shape: ', df_main.shape)
+    info_main = 'loaded main dataframe. Shape: ' + str(df_main.shape)
+    print(info_main)
     df_merge = pd.DataFrame(read_merge, columns=columns_merge_dataframe_one)
-    print('loaded merge dataframe . Shape: ', df_merge.shape)
-    print('_________________________')
+    info_merge = 'loaded merge dataframe . Shape: ' + str(df_merge.shape)
+    print(info_merge)
+    print(SEPERATE)
+
+    info_text.append(info_main)
+    info_text.append(info_merge)
+    info_text.append(SEPERATE)
 
     return df_main, df_merge
 
@@ -80,11 +97,15 @@ def merge_dataframes(df_main, df_merge):
     df = df.fillna(-99)
     # compare and recode
     df['MATCH_INFO'] = df['included_x'] + df['included_y']
-    df['MATCH_INFO'] = [mapper[i] for i in df['MATCH_INFO']]
+    df['MATCH_INFO'] = [MAPPER[i] for i in df['MATCH_INFO']]
     df_final = df
 
-    print('dataframes merged. Shape: ', df_final.shape)
-    print('_________________________')
+    info_final = 'dataframes merged. Shape: ' + str(df_final.shape)
+    print(info_final)
+    print(SEPERATE)
+
+    info_text.append(info_final)
+    info_text.append(SEPERATE)
 
     return df_final
 
@@ -93,7 +114,7 @@ def save_dataframe(df):
     print('saved dataframe: ', OUTPUT_STRING_CSV)
     df.to_excel(OUTPUT_STRING_XLSX, header=True)
     print('saved dataframe: ', OUTPUT_STRING_XLSX)
-    print('_________________________')
+    print(SEPERATE)
 
 def main():
     df_main, df_merge = load_dataframes()
@@ -101,17 +122,25 @@ def main():
     save_dataframe(df_final)
 
     match_info = df_final['MATCH_INFO'].value_counts()
+
+    info_text.append(str(match_info))
+    info_text.append(SEPERATE)
     print('Match Info')
     print(match_info)
-    print('_________________________')
-    print('Meanings')
-    print('NO_REV = not reviewed')
-    print('ONE_REV =  reviewed by one reviewer')
-    print('TWO_REV_MATCH_included = included Reviewers match')
-    print('TWO_REV_MATCH_excluded = excluded Reviewers match')
-    print('TWO_REV_DIFF = Rating differs')
+    print(SEPERATE)
+
+    for i in MEANINGS:
+        print(i)
+        info_text.append(i)
     
-
-
+    info_text.append(SEPERATE)
+        
+    with open(OUTPUT_STRING_TXT, 'w') as f:
+        for i in info_text:
+            f.write(i)
+            f.write('\n')
+           
+        f.close()
+    
 if __name__ == "__main__":
     main()
